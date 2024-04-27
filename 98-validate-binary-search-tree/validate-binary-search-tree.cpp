6ab58109d8e54ba8261c23877ca0b985b508603d
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-04-27 23:44:08 +0530

    Time: 8 ms (54.69%) | Memory: 20.2 MB (28.34%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,32 +11,53 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* root , long long int lb, long long int ub){
-        if(root==NULL){
-            return true;
-        }
+    // bool solve(TreeNode* root , long long int lb, long long int ub){
+    //     if(root==NULL){
+    //         return true;
+    //     }
 
-        if(root->val >lb && root->val <ub){
+    //     if(root->val >lb && root->val <ub){
 
-            bool leftAns=solve(root->left,lb,root->val);// left me range upper se less hoga
-            bool rightAns=solve(root->right,root->val,ub);// right me lb change hogaa
+    //         bool leftAns=solve(root->left,lb,root->val);// left me range upper se less hoga
+    //         bool rightAns=solve(root->right,root->val,ub);// right me lb change hogaa
 
-            return leftAns&&rightAns;
-        }
-        else{
-            return false;
-        }
+    //         return leftAns&&rightAns;
+    //     }
+    //     else{
+    //         return false;
+    //     }
 
         
+    // }
+    // bool isValidBST(TreeNode* root) {
+
+    //     long long int lowerBound=-4294967296; // range bcz given INT_MIN or INT_MAX ke equal bhi ho skta hai
+    //     long long int upperBound=4294967296;
+
+    //     bool ans=solve(root,lowerBound,upperBound);
+
+    //     return ans;
+    void inOrder(TreeNode* root,vector<int>&ans){
+        if(root==NULL){
+            return;
+        }
+
+        inOrder(root->left,ans);
+        ans.push_back(root->val);
+        inOrder(root->right,ans);
+     
     }
     bool isValidBST(TreeNode* root) {
+        //M-2 using Sorting Method:
+        vector<int>ans;
+        inOrder(root,ans);
 
-        long long int lowerBound=-4294967296; // range bcz given INT_MIN or INT_MAX ke equal bhi ho skta hai
-        long long int upperBound=4294967296;
-
-        bool ans=solve(root,lowerBound,upperBound);
-
-        return ans;
-        
+        // check inorder Sorted hai ya NHI::if hai the valid BST
+        for(int i=0;i<ans.size()-1;i++){
+            if(ans[i]>=ans[i+1]){
+                return false;
+            }
+        }
+        return true;
     }
 };
