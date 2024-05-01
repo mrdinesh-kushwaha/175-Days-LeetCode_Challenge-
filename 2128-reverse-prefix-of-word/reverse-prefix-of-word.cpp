19a34c3dc326207da091c51dcbe6e9cd11007672
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-05-01 22:30:00 +0530

    Time: 3 ms (42.21%) | Memory: 7.5 MB (65.62%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -2,20 +2,38 @@ class Solution {
 public:
     string reversePrefix(string word, char ch) {
         
-        int firstOcc=0;
+        // int firstOcc=0;
 
-        for(int i=0;i<word.length();i++){
-            if(word[i]==ch){
-                firstOcc=i+1;
-                break;
+        // for(int i=0;i<word.length();i++){
+        //     if(word[i]==ch){
+        //         firstOcc=i+1;
+        //         break;
+        //     }
+        // }
+        // if(firstOcc!=0){
+        // reverse(word.begin(),word.begin()+firstOcc);
+        // return word;
+        // }
+        // else{
+        //     return word;
+        // }
+
+        //**M-2 ***
+        
+        string ans="";
+
+        bool check=true;
+
+        for(auto& c:word){
+            if(c==ch && check==true){
+                ans+=c;
+                reverse(ans.begin(),ans.end());
+                check=false;
+            }
+            else{
+                ans+=c;
             }
         }
-        if(firstOcc!=0){
-        reverse(word.begin(),word.begin()+firstOcc);
-        return word;
-        }
-        else{
-            return word;
-        }
+        return ans;
     }
 };
