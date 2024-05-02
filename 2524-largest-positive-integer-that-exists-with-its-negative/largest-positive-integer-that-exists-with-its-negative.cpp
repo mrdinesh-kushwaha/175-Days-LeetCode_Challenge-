ac738c1df3bd2b6f9615cca636f403a1b8393e76
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-05-03 01:37:56 +0530

    Time: 21 ms (43.94%) | Memory: 26.7 MB (7.05%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
 
-        // int minielement=nums[0];
+        // sort(nums.begin(),nums.end());
+        // //***Using Two Pointer Approach************
         // int s=0;
         // int e=nums.size()-1;
-        // while(s<=e){
-        //     if(minielement+nums[e]==0){
-        //         return nums[e];
-        //     }
+
+        // while(s<e){
+        //     if(nums[s]+nums[e]==0)     T.C=O(nlogn)
+        //     return nums[e];
+        //     else if(nums[s]+nums[e]>0)
         //     e--;
+        //     else
         //     s++;
         // }
         // return -1;
 
-        //***Using Two Pointer Approach************
-        int s=0;
-        int e=nums.size()-1;
+        //M-2 Using Hash_map:::
 
-        while(s<e){
-            if(nums[s]+nums[e]==0)
-            return nums[e];
-            else if(nums[s]+nums[e]>0)
-            e--;
-            else
-            s++;
+        unordered_set<int>st(nums.begin(),nums.end());//insert all element in set
+        int ans=-1;
+        for(auto &num:nums){
+            if(st.find(-num)!=st.end())
+            ans=max(ans,num);
         }
-        return -1;
+        return ans;
     }
 };
