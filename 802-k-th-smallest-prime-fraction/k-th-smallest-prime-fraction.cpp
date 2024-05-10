5af191e06fa367f4e4b8bbdd818dea7c94241f90
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-05-11 00:05:15 +0530

    Time: 898 ms (17.41%) | Memory: 178.6 MB (16.25%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -1,24 +1,59 @@
+// class Solution {
+// public:
+//     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+
+//         int size=arr.size();
+//         priority_queue<vector<double>>pq; // take maxheap 
+
+//         for(int i=0;i<size-1;i++){
+//             for(int j=i+1;j<size;j++){
+//                 double div = (double)arr[i]/arr[j];
+//                 pq.push(vector<double>{div,(double)arr[i],(double)arr[j]});
+//                 if(pq.size()>k){
+//                 pq.pop();
+//                 }              //T.c=O(N^2)
+//             }
+//         }
+//         auto temp=pq.top();
+//         vector<int>ans(2);
+//         ans[0]=temp[1];
+//         ans[1]=temp[2];
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
+typedef vector<double> V;
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
 
-        int size=arr.size();
-        priority_queue<vector<double>>pq; // take maxheap 
+        int n=arr.size();
+        priority_queue<V,vector<V>,greater<V>> pq; // min heap {fraction,i,j}
 
-        for(int i=0;i<size-1;i++){
-            for(int j=i+1;j<size;j++){
-                double div = (double)arr[i]/arr[j];
-                pq.push(vector<double>{div,(double)arr[i],(double)arr[j]});
-                if(pq.size()>k){
-                pq.pop();
-                }
-            }
+        for(int i=0;i<n;i++){
+            pq.push({(double)arr[i]/arr[n-1],(double)i,(double)n-1});
         }
-        auto temp=pq.top();
-        vector<int>ans(2);
-        ans[0]=temp[1];
-        ans[1]=temp[2];
 
-        return ans;
+        int smallest=1;
+        while(smallest<k){
+            
+            V temp=pq.top();
+            pq.pop();
+
+            int i=temp[1]; //index
+            int j=temp[2]-1; //index
+
+            pq.push({(double)arr[i]/arr[j],(double)i,(double)j});
+
+            smallest++;
+        }
+        V temp=pq.top();
+        int i=temp[1];
+        int j=temp[2];
+
+        return {arr[i],arr[j]};
+
     }
+
 };
