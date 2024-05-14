b6e1ea63aa77cfdc185532d0278bebdbe74e9b88
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-05-15 00:42:44 +0530

    Time: 149 ms (53.42%) | Memory: 9.2 MB (53.68%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    vector<vector<int>>directions{{-1,0},{1,0},{0,1},{0,-1}};
     int dfs(vector<vector<int>>& grid, int i, int j) {
     // Boundary conditions
     int row = grid.size();
@@ -7,8 +8,6 @@ public:
     if (i >= row || i < 0 || j >= col || j < 0 || grid[i][j] == 0) {
         return 0;
     }
-
-    int sum = 0;
     int maxGold = 0;
     int temp = grid[i][j];
     //sum += temp;
@@ -16,19 +15,25 @@ public:
     // Mark current cell as visited
     grid[i][j] = 0;
 
+    for(vector<int>&dir:directions){
+        int new_i= i+dir[0];
+        int new_j= j+ dir[1];
+
+        maxGold=max(maxGold,dfs(grid,new_i ,new_j));
+    }
     // Explore adjacent cells
-    int up = dfs(grid, i - 1, j);
-    int down = dfs(grid, i + 1, j);
-    int left = dfs(grid, i, j - 1);
-    int right = dfs(grid, i, j + 1);
+    // int up = dfs(grid, i - 1, j);
+    // int down = dfs(grid, i + 1, j);
+    // int left = dfs(grid, i, j - 1);
+    // int right = dfs(grid, i, j + 1);
 
     // Restore current cell
     grid[i][j] = temp;
 
     // Calculate maxGold from all possible paths
-    maxGold = max({ maxGold, up, down, left, right }) + temp;
+   // maxGold = max({ maxGold, up, down, left, right }) + temp;
 
-    return maxGold;
+    return maxGold+temp;
 }
 
 int getMaximumGold(vector<vector<int>>& grid) {
