210df92624a4c740ee40f4c1bbbe3a4ecd535462
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-05-18 13:16:14 +0530

    Time: 29 ms (6.23%) | Memory: 20.4 MB (6.09%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,5 +1,100 @@
+class TrieNode{
+    public:
+    char data;
+    bool isTerminal;
+    TrieNode* children[26];
+    int childcount;
+
+    TrieNode(char d){
+        this->data=d;
+        isTerminal=false;
+        childcount=0;
+        for(int i=0;i<26;i++){
+            children[i]=NULL;
+        }
+       
+    }
+};
 class Solution {
 public:
+
+    void insertTrieNode(TrieNode* root, string word){  // Insert TrieNode
+
+    //cout<<"Inserting "<<word<<endl;
+    //base case:
+    if(word.size()==0){
+
+        // means word end ho gya then mark isterminal as True:
+        root->isTerminal=true;
+        return;
+    }
+
+    char ch= word[0];
+    int index=ch-'a';
+    TrieNode* child;
+
+    // check present 
+    if(root->children[index]!=NULL){
+        child=root->children[index];
+    }  
+    else{ // present nhi hai:
+        root->childcount++;
+        child=new TrieNode(ch);
+        root->children[index]=child;
+    }
+
+    // recursion call
+    insertTrieNode(child,word.substr(1));
+}
+
+bool findWord(TrieNode* root,string word){ // Searching TrieNode
+
+    //base case
+    if(word.size()==0){
+        return root->isTerminal;
+    }
+
+    char ch=word[0];
+    int index=ch-'a';
+
+    TrieNode * child;
+    if(root->children[index]!=NULL){
+        // nove
+        child=root->children[index];
+    }
+    else{
+        return false;
+    }
+
+    // call recursion
+    return findWord(child,word.substr(1));
+ 
+   }
+
+   void LCA(TrieNode* root,string first,string &ans){
+
+    if(root->isTerminal) // String NULL ho jaye 
+        return ;
+        
+    for(int i=0;i<first.length();i++){
+        char ch=first[i];
+
+        if(root->childcount==1){
+            ans.push_back(ch);
+            int index=ch-'a';
+            root=root->children[index];
+        }
+        else{
+            break;
+        }
+        if(root->isTerminal){
+            break;
+        }
+    }
+
+   }
+
+
     string longestCommonPrefix(vector<string>& s) {
 
     //  string ans;
@@ -15,23 +110,45 @@ public:
     //  }
     //   return ans;
 
-    string ans="";
-    bool match=true;
 
-    for(int i=0;i<s[0].length();i++){
-        char ch=s[0][i];  // first string ke char ko point kr rha hai::
+//*************M-2****************************//
 
-        for(int j=1;j<s.size();j++){
-            if(ch!=s[j][i]){
-                match=false;
-               break;
-            }
+    // string ans="";
+    // bool match=true;
+
+    // for(int i=0;i<s[0].length();i++){
+    //     char ch=s[0][i];  // first string ke char ko point kr rha hai::
+
+    //     for(int j=1;j<s.size();j++){
+    //         if(ch!=s[j][i]){
+    //             match=false;
+    //            break;
+    //         }
+    //     }
+    //     if(match!=true)
+    //     break;
+    //     else
+    //     ans+=ch;
+    // }
+    // return ans;
+
+    //Method- 3 Using Trie Data Structure:::::::::::************
+
+        //Insert Each string in TrieNode
+        TrieNode* root=new TrieNode('-');
+
+        for(int i=0;i<s.size();i++){
+            insertTrieNode(root,s[i]);
         }
-        if(match!=true)
-        break;
-        else
-        ans+=ch;
-    }
-    return ans;
+
+        string ans="";
+        string first=s[0];
+
+        LCA(root,first,ans);
+
+        return ans;
+
+
+
     }
 };
