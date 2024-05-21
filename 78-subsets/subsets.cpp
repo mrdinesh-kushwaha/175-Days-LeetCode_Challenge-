88780ddb736578779c5ac9b15b6b03567b49f89c
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-05-22 01:52:11 +0530

    Time: 4 ms (39.73%) | Memory: 8.2 MB (94.65%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -9,8 +9,8 @@ public:
             int n=ans.size();
             for(int i=0;i<n;i++){
                 ans.push_back(ans[i]);
-                ans.back().push_back(num);
-            }
+                ans.back().push_back(num); //means ans ke last vale element me jao aur usme add kr do
+            }                                // eg: {},{1} ->{1,2} if num=2 
         }
         return ans;
     }
