bad603af4f4c8a6d339d6ae39b2ac590b4b3993e
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-05-28 02:29:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (19.83%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
-        
+        sort(nums.begin(),nums.end());
         int n=nums.size();
         int x=0;
         for(int i=0;i<=nums.size();i++){
             x=i;
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(nums[j]> x || nums[j]==x){
-                    count++;
-                }
-            }
+            int idx=lower_bound(nums.begin(),nums.end(),x)-nums.begin();
+            // int count=0;
+            // for(int j=0;j<n;j++){
+            //     if(nums[j]> x || nums[j]==x){
+            //         count++;
+            //     }
+            // }
+            int count=n-idx;
             if(x==count) return x;
 
         }
