5c798c58577fdd372746280fec88158bc0c603a7
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-06-04 09:53:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (26.04%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<int,int>mp;
+        unordered_map<char,int>mp;
         for(int i=0;i<s.length();i++){
-            mp[s[i]-'0']++;
+            mp[s[i]]++;
         }
         int ans=0;
         bool f=false;
@@ -17,8 +17,8 @@ public:
                 ans+=val;
             }
         }
-        if(f==true)
-        return 1+ ans;
+        if(f==true)  // means you have #odd then do otherwise return only ans:::
+        return 1+ ans; // '+1' bzc in any palidrome we can insert one char in between :: eg. abbcbba
         else
         return ans;
     }
