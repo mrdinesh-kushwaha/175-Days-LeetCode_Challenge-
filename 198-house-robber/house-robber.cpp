55537c1a9de992560bf3ccf43047bfea512b47d3
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-06-04 18:01:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (59.69%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,17 +1,40 @@
-class Solution {
+// class Solution {
+// public:
+// int solve(int idx,vector<int>&nums,vector<int>&dp){ // memoizations::
+//     if(idx>=nums.size()) return 0;
+//     if(dp[idx]!=-1) return dp[idx];
+//     // pick
+//     int left = nums[idx]+solve(idx+2,nums,dp);
+//     // none pick
+//     int right = 0+ solve(idx+1,nums,dp);
+
+//     return dp[idx]=max(left,right);  // same as adjacent sum :max
+// }
+//     int rob(vector<int>& nums) {
+//         vector<int>dp(nums.size()+1,-1);
+//         return solve(0,nums,dp);
+//     }
+// };
+
+class Solution { // tebulations t.c= O(N) s.c= O(N) dp array
 public:
 int solve(int idx,vector<int>&nums,vector<int>&dp){
-    if(idx>=nums.size()) return 0;
-    if(dp[idx]!=-1) return dp[idx];
-    // pick
-    int left = nums[idx]+solve(idx+2,nums,dp);
-    // none pick
-    int right = 0+ solve(idx+1,nums,dp);
 
-    return dp[idx]=max(left,right);
+    dp[0]=nums[0];
+
+    for(int i=1;i<nums.size();i++){
+        int temp=0;
+        if(i-2>=0) temp=dp[i-2];
+
+        int take= nums[i]+ temp;
+        int nontake= 0+ dp[i-1];
+
+        dp[i]=max(take,nontake);
+    }
+    return dp[nums.size()-1]; // same as adjacent sum :max
 }
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size()+1,-1);
+        vector<int>dp(nums.size(),0);
         return solve(0,nums,dp);
     }
 };
