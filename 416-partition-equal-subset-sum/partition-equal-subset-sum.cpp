2c1bd5083a48c3ab8f69e69a65f9c122b5fa71df
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-06-05 19:22:13 +0530

    Time: 150 ms (53.23%) | Memory: 99.3 MB (22.37%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -16,13 +16,34 @@ bool solve(int idx,vector<int>&nums,int target,vector<vector<int>>&dp){
         return dp[idx][target];
 
 }
+bool getEqaulSubsetSum(vector<int>&nums,int target){ // space optimise tebulation (bootom-up)
+    int n=nums.size();
+    vector<vector<int>>dp(n+1,vector<int>(target+1,0));
+    // when target(col) ->0 then val 1
+    for(int idx=0;idx<n;idx++){
+        dp[idx][0]=1;
+    }
+
+    for(int idx=n-1;idx>=0;idx--){
+        for(int t=1;t<=target;t++){
+        bool take=0;
+        if(t-nums[idx]>=0) take= dp[idx+1][t-nums[idx]];
+        bool nontake= dp[idx+1][t];
+
+        dp[idx][t] = (take||nontake) ;
+        }
+    }
+    return dp[0][target];
+}
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
         for(auto i:nums) sum+=i;
         if(sum & 1) return false;
         int target=sum/2;
-        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
-        return solve(0,nums,target,dp);
+        // vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        // return solve(0,nums,target,dp);
+
+        return getEqaulSubsetSum(nums,target);
     }
 };
