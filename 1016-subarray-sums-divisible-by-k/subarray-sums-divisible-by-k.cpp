2c10e5313bb7285bd8d1b9ed4148f3e36142be1a
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-06-09 17:18:14 +0530

    Time: 40 ms (61.84%) | Memory: 35.1 MB (44.61%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
 
-        unordered_map<int,int>prefix_sum;
-        prefix_sum[0]=1; // base case:
+        unordered_map<int,int>prefix_sum_Freq;
+        prefix_sum_Freq[0]=1; // base case:
         int sum=0;
         int ans=0;
 
@@ -14,10 +14,10 @@ public:
             if(mod<0){
                 mod= (mod % k + k); // convert negative reminder into possitive reminder
             }
-            if(prefix_sum.find(mod)!=prefix_sum.end()){
-                ans+=prefix_sum[mod]; //add freq of prefix_sum
+            if(prefix_sum_Freq.find(mod)!=prefix_sum_Freq.end()){
+                ans += prefix_sum_Freq[mod]; //add freq of prefix_sum
             }
-            prefix_sum[mod]++; // increase freq:
+            prefix_sum_Freq[mod]++; // increase freq:
         }
         return ans;
     }
