7f0cb3be39ff2d0eeb01d714200027db6be714ed
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-06-13 23:56:19 +0530

    Time: 9 ms (37.04%) | Memory: 21.1 MB (17.35%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,15 +1,46 @@
+// class Solution {
+// public:
+//     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+
+//         sort(begin(seats),end(seats));
+//         sort(begin(students),end(students));
+
+//         int ans=0;
+//         int n= seats.size();
+//         for(int i=0;i<n;i++){
+//             ans+=abs(seats[i]-students[i]);   // T.C= O(n*logn)
+//         }                                     // S.C = O(1)
+//         return ans;   
+//     }
+// };
+
+// Method-2:
+
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
 
-        sort(begin(seats),end(seats));
-        sort(begin(students),end(students));
+        //using taking two extra space::
+        vector<int>freq_seats(101,0); // this is the freq array: store the freq of seats
+        vector<int>freq_students(101,0); // this is the freq array: store the freq of students
 
-        int ans=0;
-        int n= seats.size();
-        for(int i=0;i<n;i++){
-            ans+=abs(seats[i]-students[i]);
-        }
-        return ans;   
+        for(int &i :seats) freq_seats[i]++;
+        for(int &i: students) freq_students[i]++;
+
+        int i=0,j=0,ans=0;
+        int n=seats.size();
+        while(n>0){
+            if(freq_seats[i]==0) i++;
+            if(freq_students[j]==0) j++;
+
+            if(freq_seats[i]!=0 && freq_students[j]!=0){
+                ans+=abs(j-i);
+                freq_seats[i]--; // decrease freq by 1:
+                freq_students[j]--; // decrease freq by 1:
+                n--; 
+            }
+        }                        // T.C= O(n)
+                                 // S.c= O(101)
+        return ans;
     }
 };
