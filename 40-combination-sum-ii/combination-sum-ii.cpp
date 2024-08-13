62f408fa2deb1450633d4fa4969b014ea45a6a57
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-08-13 08:45:52 +0530

    Time: 4 ms (60.85%) | Memory: 14.1 MB (23.77%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    void solve(vector<int>& candidates, int idx, vector<int> temp, vector<vector<int>>& ans, int target) {
-        if(target == 0) {
+    void solve(vector<int>& arr, int target,int idx,vector<int>temp,vector<vector<int>>&ans){
+        //base case:
+        if(target==0){
             ans.push_back(temp);
             return;
         }
-        
-        for(int i = idx; i < candidates.size(); i++) {
-            if(i > idx && candidates[i] == candidates[i-1]) continue; // Skip duplicates
-            if(candidates[i] > target) break; // No need to proceed further if the current number exceeds the target
-            
-            temp.push_back(candidates[i]);
-            solve(candidates, i+1, temp, ans, target - candidates[i]);
-            temp.pop_back();
+        for(int i=idx;i<arr.size();i++){
+            if(i>idx && arr[i]==arr[i-1]) continue;
+            if(arr[i] > target) break;
+
+            temp.push_back(arr[i]); // store solution 
+            solve(arr,target-arr[i],i+1,temp,ans); //call recursion for next cobination
+
+            temp.pop_back(); // pop the element for remain same 
         }
     }
-
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end()); // Sort the candidates to handle duplicates
-        vector<int> temp;
-        vector<vector<int>> ans;
-        solve(candidates, 0, temp, ans, target);
+        vector<int>temp;
+        vector<vector<int>>ans; // for store all the combination
+        sort(begin(candidates),end(candidates));
+        solve(candidates,target,0,temp,ans);
+
         return ans;
     }
 };
