7534ef829ebde81fb693a72aa9b329eca51a7aed
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-08-26 19:27:50 +0530

    Time: 14 ms (51.70%) | Memory: 15.2 MB (49.47%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
@@ -20,19 +20,16 @@ public:
 
 class Solution {
 public:
-    void solve(Node* root,vector<int>& ans){
+    void solve(Node* root , vector<int>&ans){
         if(root==NULL) return ;
-
-        for(Node* child: root->children){
+        for(Node* child:root->children){
             solve(child,ans);
         }
         ans.push_back(root->val);
     }
     vector<int> postorder(Node* root) {
-
         vector<int>ans;
         solve(root,ans);
-
         return ans;
     }
 };
