c739e6443b79afb67a2f3c2883baf67dc51061a5
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-09-01 19:54:00 +0530

    Time: 64 ms (97.66%) | Memory: 92.7 MB (37.03%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        // vector<vector<int>>result(m,vector<int>(n));
+        // //edages case::
+        // int size=original.size();
+        // if(m*n!=size) return {};
+
+        // int p=0; // used for access original array element //T.C//O(m*n)
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //        result[i][j]=original[p++];
+        //     }
+        // }
+        // return result;
+
+        //M-2
+
         vector<vector<int>>result(m,vector<int>(n));
-        //edages case::
         int size=original.size();
         if(m*n!=size) return {};
 
-        int p=0; // used for access original array element
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-               result[i][j]=original[p++];
-            }
+        for(int i=0;i<size;i++){
+            int row=i/n; // find row
+            int col=i%n; // find col
+            result[row][col]=original[i];
         }
         return result;
     }
