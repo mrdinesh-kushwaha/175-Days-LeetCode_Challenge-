dbcc99a313cfa93fb7c8dc80c6ec8ecdd5538891
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-09-19 02:10:39 +0530

    Time: 2 ms (55.15%) | Memory: 8.9 MB (45.53%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -1,24 +1,28 @@
+
+#include <sstream>
 class Solution {
 public:
-    map<string,int>mp;
+    vector<string> uncommonFromSentences(string s1, string s2) {
+
+    vector<string> res; 
+    unordered_map<string,int> mp;
     
-    void stringstreeam(string s)
-    {
-        stringstream ss(s);
-        string word;
-        while(ss>>word) mp[word]++;
+    // Concatenate the two sentences
+    s1 = s1 + " " + s2; // add or Merge both sentence::: in string s1
+
+    stringstream ss(s1); //use for split in words:
+    string word;
+    // Split the concatenated string and count the occurrence of each word
+    while(ss >> word){ // convert s1 sentence into words::: and store into mp
+        mp[word]++;
     }
 
-    vector<string> uncommonFromSentences(string s1, string s2) 
-    {
-        stringstreeam(s1);
-        stringstreeam(s2);
-
-        vector<string>ans;
-        for(auto x:mp)
-            if(x.second==1) 
-                ans.push_back(x.first);
-
-        return ans;
+    // Add words that appear only once to the result
+    for (auto i : mp) {
+        if (i.second == 1) {
+            res.push_back(i.first);
+        }
+    }
+    return res;
     }
 };
