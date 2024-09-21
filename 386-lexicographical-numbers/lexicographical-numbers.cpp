e4c45e76d51cb56e5b01a2348232fa872e44bbc6
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-09-21 15:41:31 +0530

    Time: 5 ms (81.41%) | Memory: 13.6 MB (76.44%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,17 +1,41 @@
 class Solution {
 public:
-    vector<int> lexicalOrder(int n) {
-        
-        vector<string>res;
-        for(int i=1;i<=n;i++){
-            res.push_back(to_string(i));
+    // vector<int> lexicalOrder(int n) {
+         
+    //      // brute force Approach...................
+
+    //     // vector<string>res;
+    //     // for(int i=1;i<=n;i++){
+    //     //     res.push_back(to_string(i));
+    //     // }
+    //     // sort(begin(res),end(res)); //sort according to the lexico-graphical order :::
+
+    //     // vector<int>ans;
+    //     // for(auto& i:res)
+    //     // ans.push_back(stoi(i)); 
+
+    //     // return ans;
+
+    //     //M-2 ..............
+
+    // }
+    void solve(int currNum,int& n,vector<int>&ans){
+        if(currNum > n) return ; // if CurrNum N se graeter ho jaye then aa jao
+        ans.push_back(currNum);
+
+        for(int append=0;append<=9;append++){
+            int newNum= currNum*10 + append; // all possible in lexicographical order.....
+
+            if(newNum > n) return; // if newNum Nse greater Ho jaye....
+            solve(newNum,n,ans); //Find all possible...........
         }
-        sort(begin(res),end(res)); //sort according to the lexico-graphical order :::
-
+    }
+      vector<int> lexicalOrder(int n) {
+         
         vector<int>ans;
-        for(auto& i:res)
-        ans.push_back(stoi(i)); 
-
+        for(int i=1;i<=9;i++){
+            solve(i,n,ans);
+        }
         return ans;
     }
 };
