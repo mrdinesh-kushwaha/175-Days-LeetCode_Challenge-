099d0c732f864c44700721d3d2dca686fbe48b6d
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-10-16 19:10:58 +0530

    Time: 3 ms (43.32%) | Memory: 7.9 MB (30.30%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.cpp
+++ 1304-longest-happy-string/longest-happy-string.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
+        // Solve using priority_queue : max
+        // Step-1 
+        priority_queue<pair<int,char>>pq; // initialize max priority_queue
+        if(a>0) pq.push({a,'a'});
+        if(b>0) pq.push({b,'b'});
+        if(c>0) pq.push({c,'c'});
 
-        priority_queue<pair<int,char>>pq;
-
-        if(a>0){
-            pq.push(make_pair(a,'a'));
-        }
-        if(b>0){
-            pq.push(make_pair(b,'b'));
-        }
-        if(c>0){
-            pq.push(make_pair(c,'c'));
-        }
+        //Step-2 Traverse in Queue
         string ans="";
         while(pq.size()>1){
-            auto temp1=pq.top();
+            auto temp1= pq.top();
             pq.pop();
-            auto temp2=pq.top();
+            auto temp2= pq.top();
             pq.pop();
 
-            if(temp1.first>=2){
+            if(temp1.first >=2){
                 ans+=temp1.second;
                 ans+=temp1.second;
                 temp1.first-=2;
@@ -39,23 +35,15 @@ public:
                 ans+=temp2.second;
                 temp2.first--;
             }
-
-            if(temp1.first>0){
-                pq.push(temp1);
-            }
-            if(temp2.first>0){
-                pq.push(temp2);
-            }
-            
+            if(temp1.first > 0) pq.push(temp1);
+            if(temp2.first > 0) pq.push(temp2);
         }
-
-        if(!pq.empty()){
-            auto temp=pq.top();
+        if(pq.size()>0){
+            auto temp = pq.top();
             pq.pop();
-
-            if(temp.first>=2){
+            if(temp.first >=2 ){
                 ans+=temp.second;
-                ans+=temp.second;
+                ans+= temp.second;
                 temp.first-=2;
             }
             else{
@@ -63,6 +51,6 @@ public:
                 temp.first--;
             }
         }
-        return ans;
+    return ans;
     }
 };
