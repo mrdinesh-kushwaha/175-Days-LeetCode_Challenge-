d2cd1ec1a9013a574c1c23d975d37da63c3e2d00
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-11-19 19:12:58 +0530

    Time: 121 ms (39.43%) | Memory: 98.3 MB (42.60%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    long long maximumSubarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        long long result=0;
-        unordered_set<int>st;
-        long long currWindowSum=0;
-        int i=0;
-        int j=0;
-
-        while(j<n){
-            // if deplicate exist
-            while(st.count(nums[j])){
-                currWindowSum -= nums[i];
-                st.erase(nums[i]);
-                i++;
-            }
-            currWindowSum +=nums[j];
-            st.insert(nums[j]);
-            // if length equal to the k:
-            if(j-i+1 == k){
-                result=max(result,currWindowSum);
-                currWindowSum -=nums[i];
-                st.erase(nums[i]);
-                i++;
-            }
-            j++;
+    long long maximumSubarraySum(vector<int>& n, int k) {
+   long long m = 0, s = 0;
+    int l = 0;
+    unordered_map<int, int> c;
+    for (int r = 0; r < n.size(); r++) {
+        c[n[r]]++;
+        s += n[r];
+        while (c[n[r]] > 1 || r - l + 1 > k) {
+            c[n[l]]--;
+            s -= n[l];
+            l++;
         }
-        return result;
+        if (r - l + 1 == k) m = max(m, s);
+    }
+    return m;
+
     }
 };
