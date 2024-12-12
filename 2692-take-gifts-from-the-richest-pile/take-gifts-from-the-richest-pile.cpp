ed8c0042b37b49c0825a8a959a208bc1d9123c9a
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-12-12 10:01:38 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (89.48%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -2,20 +2,47 @@ class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
         int n=gifts.size();
-        while(k--){
-            int idx = 1;
-            int maxi=INT_MIN;
-            for(int i=0;i<n;i++){
-                if(maxi<=gifts[i]){
-                maxi=max(maxi,gifts[i]);
-                idx=i;
-                }
-            }
-            gifts[idx]=sqrt(maxi);
-        }
-        long long sum=0;
-        for(auto& i: gifts) sum+=i;
+        // while(k--){
+        //     int idx = 1;
+        //     int maxi=INT_MIN;
+        //     for(int i=0;i<n;i++){                   // T.C= O(k*N), S.C=O(1)
+        //         if(maxi<=gifts[i]){
+        //         maxi=max(maxi,gifts[i]);
+        //         idx=i;
+        //         }
+        //     }
+        //     gifts[idx]=sqrt(maxi);
+        // }
 
-        return sum;
+        //  while(k--){
+            
+
+        // }
+        // long long sum=0;
+        // for(auto& i: gifts) sum+=i;
+
+        // return sum;
+
+        //************** optimise T.C = O(k) , S.C = O(N)
+
+        priority_queue<long long>pq;
+
+        for(auto& i:gifts){ // insert all ele in pariority queue
+            pq.push(i);
+        }
+
+        while(k--){
+            int maxi=pq.top();
+            pq.pop();
+            pq.push(sqrt(maxi));
+        }
+
+        long long ans=0;
+        while(!pq.empty()){
+            ans+=pq.top();
+            pq.pop();
+        }
+
+        return ans;
     }
 };
