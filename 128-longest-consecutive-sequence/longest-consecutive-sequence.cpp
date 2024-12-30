10668c3f48e57a5f4b41ca66830720d244ce82ef
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2024-12-30 16:28:58 +0530

    Time: 792 ms (24.23%) | Memory: 74.9 MB (25.11%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        // It's Take n(logN) time complexity need to optimise it:
+        // SPace complexity = O(1)
+        // int n = nums.size();
+        // if(n==0) return 0;
+        // sort(begin(nums),end(nums));
+        // int cnt=1;
+        // int result = INT_MIN;
+        // for(int i=0;i<n-1;i++)
+        // {
+        //     if(nums[i]==nums[i+1]) continue; // used for avoid depliacte:
+        //     if((nums[i]+1) == nums[i+1]) cnt++; 
+        //     if((nums[i]+1) != nums[i+1]){
+        //         result=max(result,cnt);
+        //         cnt=1;
+        //     }
+        // }
+        // result=max(cnt,result);  
+        // return result;
+
+        // *******Now Optimise Time complexity = O(N)
+        // Space complexity = O(N)
+        // Now solve using Hash set
         int n = nums.size();
-        if(n==0) return 0;
-        sort(begin(nums),end(nums));
-        int cnt=1;
-        int result = INT_MIN;
-        for(int i=0;i<n-1;i++)
-        {
-            if(nums[i]==nums[i+1]) continue;
-            if((nums[i]+1) == nums[i+1]) cnt++; 
-            if((nums[i]+1) != nums[i+1]){
-                result=max(result,cnt);
-                cnt=1;
+        unordered_set<int>st;
+        for(auto i: nums){
+            st.insert(i);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(st.find(nums[i]-1)!=st.end()) continue;
+            else{
+                int cnt = 0;
+                int num = nums[i];
+                while(st.find(num)!=st.end()){
+                    cnt++;
+                    num++;
+                }
+                ans=max(ans,cnt);
             }
         }
-        result=max(cnt,result);  
-        return result;
+        return ans;
     }
 };
