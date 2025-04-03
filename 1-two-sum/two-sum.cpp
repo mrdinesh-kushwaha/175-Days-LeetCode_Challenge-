58ba73ff54b3f7156477cd71a5f8a663dc323883
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2025-04-04 00:53:51 +0530

    Time: 8 ms (47.03%) | Memory: 15.8 MB (10.22%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,11 +2,23 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
     // Brute Force Approach
-    for(int i=0;i<nums.size()-1;i++){
-        for(int j=i+1;j<nums.size();j++){
+    // for(int i=0;i<nums.size()-1;i++){
+    //     for(int j=i+1;j<nums.size();j++){
 
-            if((nums[i]+nums[j])==target) return{i,j};
-        }
+    //         if((nums[i]+nums[j])==target) return{i,j};
+    //     }
+    // }
+    // return {-1,-1};
+
+    // Approach-2 Hash Map
+    unordered_map<int,int>mp;
+    for(int i=0;i<nums.size();i++){
+        mp[nums[i]]=i;
+    }
+    for(int i=0;i<nums.size();i++){
+        int ele=target-nums[i];
+
+        if(mp.find(ele)!=mp.end() && mp[ele]!=i) return {i,mp[ele]};
     }
     return {-1,-1};
     }
