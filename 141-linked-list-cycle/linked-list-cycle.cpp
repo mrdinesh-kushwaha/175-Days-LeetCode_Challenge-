2dd895260925dc916e415c229d4e5446d62a9773
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2026-01-03 22:25:21 +0530

    Time: 13 ms (16.08%) | Memory: 11.8 MB (51.90%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -8,39 +8,21 @@
  */
 class Solution {
 public:
-    // bool hasCycle(ListNode *head) {
-    //     if(head==NULL||head->next==NULL)
-    //     return false;
-    //     ListNode* slow=head;
-    //     ListNode* fast=head;
-    //     while(fast->next!=NULL){
-    //         fast=fast->next;
-    //         if(fast->next!=NULL)
-    //         {
-    //             fast=fast->next;
-    //             slow=slow->next;
-    //         }
-    //         if(slow==fast)
-    //         return true;
-    //     }
-    //     return false;
-    // }
-
-    // USING MAP::
-
-     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*,bool>mp;
-
-        ListNode* temp=head;
-        while(temp){
-            if(mp.find(temp)==mp.end()){
-                mp[temp]=true;
-            }
-            else{// again mil gya means loop present
-                return true;
-            }
-            temp=temp->next;
+    bool hasCycle(ListNode *head) {
+        if(head==NULL || head->next == NULL){
+            return false;
         }
+
+        ListNode* slow = head;
+        ListNode* fast  = head;
+
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow==fast) return true;
+        }
+
         return false;
     }
 };
