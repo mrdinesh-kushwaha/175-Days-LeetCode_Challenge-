a271f14ff6ba7674185a0224b398066539e38734
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2026-02-27 11:17:05 +0530

    Time: 35 ms (98.52%) | Memory: 29.1 MB (45.60%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,62 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>>result;
-    void twoSum(vector<int>&nums,int target,int i,int j){
-        while(i<j){
-            if(nums[i]+nums[j] < target) i++;
-            else if(nums[i]+nums[j]>target) j--;
-            else{
-                //remove deplicate first
-                while(i<j && nums[i]==nums[i+1]) i++;
-                while(i<j && nums[j]==nums[j-1]) j--;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        //Extended Version Sum
+        //nums[i]+nums[left]+nums[right]=0
+        //nums[left]+nums[right] = -nums[i] -> let's suppose target
+        //nums[left] + nums[right] = target
 
-                result.push_back({-target,nums[i],nums[j]});
-                i++,j--;
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        vector<vector<int>>res; // for result
+
+        for(int i=0;i<nums.size()-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int left = i+1;
+            int right = nums.size()-1;
+            int target = -nums[i];
+            int pointerSum = 0;
+
+            while(left<right){
+                pointerSum = nums[left]+nums[right];
+                vector<int>temp;
+                if(pointerSum == target){
+                    //store
+                    res.push_back({nums[i],nums[left],nums[right]});
+                    left++;
+                    right--;
+
+                    while(left<n && nums[left] == nums[left-1]) left++;
+                    while(right>=0 && nums[right] == nums[right+1]) right--;
+                }
+                else if(target > pointerSum) left++;
+                else right--; 
             }
         }
-    }
-    vector<vector<int>> threeSum(vector<int>& nums) {
-
-        // int target = 0;
-        // sort(nums.begin(), nums.end());
-        // set<vector<int>> s;
-        // vector<vector<int>> output;
-        // for (int i = 0; i < nums.size(); i++){
-        //     int j = i + 1;
-        //     int k = nums.size() - 1;
-        //     while (j < k) {
-        //         int sum = nums[i] + nums[j] + nums[k];
-        //         if (sum == target) {
-        //             s.insert({nums[i], nums[j], nums[k]});
-        //             j++;
-        //             k--;
-        //         } else if (sum < target) {
-        //             j++;
-        //         } else {
-        //             k--;
-        //         }
-        //     }
-        // }
-        // for(auto triplets : s)
-        //     output.push_back(triplets);
-        // return output;
-
-        //***Solve this problem convert into TwoSum Problem
-        // n1+n2+n3=0 -> n2+n3 = -n1(target)
-        result.clear();
-        int n=nums.size();
-        if(n < 3) return {};
-        sort(begin(nums),end(nums));
-
-        for(int i=0;i<n-2;i++){
-            if(i > 0 && nums[i]==nums[i-1]) continue;
-
-            int n1 = nums[i];
-            int target = -n1;
-
-            twoSum(nums,target,i+1,n-1);
-        }
-        return result;
-    
+        return res;
     }
 };
