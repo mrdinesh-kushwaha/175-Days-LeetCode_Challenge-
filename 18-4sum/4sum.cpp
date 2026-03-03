1e45ed8772d18b0f006f50918f27deb30e0b28c9
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2026-03-03 11:22:06 +0530

    Time: 22 ms (59.22%) | Memory: 17.3 MB (83.29%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-         // Solve this problem using the 3Sum problem Approach
-        // using two pointer approach to solve this problem
-
         int n = nums.size();
-        sort(begin(nums),end(nums));
-        vector<vector<int>>ans; // for storing the result
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue ; // for remove deplicate
-            for(int j=i+1;j<n;j++){
-                if(j != i+1 && nums[j]==nums[j-1]) continue;// for remove deplicate
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-3;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            
+            for(int j=i+1;j<n-2;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
 
-                //Now apply Two Pointer Approach
                 int left = j+1;
                 int right = n-1;
-                while(left<right){
-                    long long sum = nums[i];
-                    sum+=nums[j];
-                    sum+=nums[left];
-                    sum+=nums[right];
-                    if(sum==target){
-                        vector<int>temp={nums[i],nums[j],nums[left],nums[right]};
-                        ans.push_back(temp);
-                        left++,right--;
 
-                        //Now also Need to remove deplicate
-                        while(left<right && nums[left]==nums[left-1])left++;
-                        while(left<right && nums[right]==nums[right+1])right--;
+                //Now 2-pointer Approach
+                while(left<right){
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[left] + nums[right];
+                    if(sum==target){
+                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+                        left++;
+                        right--;
+
+                        //for deplicate
+                        while(left<n && nums[left]==nums[left-1]) left++;
+                        while(right>=0 && nums[right]==nums[right+1]) right--;
                     }
-                    else if(sum > target) right--;
-                    else left++;
+                    else if(sum < target) left++;
+                    else right--;
                 }
             }
         }
