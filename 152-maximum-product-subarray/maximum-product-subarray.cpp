4d7b6fdef1e04466d9e83c1ca37c83406921419f
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2026-03-23 22:54:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (22.71%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -4,21 +4,35 @@ public:
         //Using Kadance Algorithm
         //At each index 3 option
         //1- curr i ko lekar alag subarray banao
-        //2- i*minProd
-        //3- i*maxProd
+        //2- nums[i]*minProd
+        //3- nums[i]*maxProd
 
+        // int minProd = nums[0];
+        // int maxProd = nums[0];
+        // int res = nums[0];
+
+        // for(int i=1;i<nums.size();i++){
+        //     int curr = nums[i];
+        //     int v1 = nums[i]*minProd;
+        //     int v2 = nums[i]*maxProd;
+
+        //     minProd = min(curr, min(v1,v2));
+        //     maxProd = max(curr, max(v1,v2));
+
+        //     res = max(res, max(minProd, maxProd));
+        // }
+        // return res;
+
+        //Alternative Approach Using Swap
         int minProd = nums[0];
         int maxProd = nums[0];
         int res = nums[0];
 
         for(int i=1;i<nums.size();i++){
             int curr = nums[i];
-            int v1 = nums[i]*minProd;
-            int v2 = nums[i]*maxProd;
-
-            minProd = min(curr, min(v1,v2));
-            maxProd = max(curr, max(v1,v2));
-
+            if(curr<0) swap(minProd,maxProd);
+            minProd = min(curr, curr*minProd);
+            maxProd = max(curr, curr*maxProd);
             res = max(res, max(minProd, maxProd));
         }
         return res;
