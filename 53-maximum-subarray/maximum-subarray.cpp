a94ca60729f357cd5cabb8017f55084a4071e9c8
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2026-03-23 20:29:16 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (52.10%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,34 +1,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-    //     int n = nums.size();
-    //     int sum=INT_MIN;
-    //     for(int i=0;i<n;i++){
-    //         int temp=0;
-    //         for(int j=i;j<n;j++){
-    //             temp+=nums[j];
-    //             sum=max(sum,temp);
-    //         }
-    //     }
-    // return sum;
+        int curr = nums[0]; //best ending
+        int res = nums[0];
 
-    //*** Kanden's Algorithm
-
-    int result=INT_MIN;
-    int sum=0;
-
-    for(int i=0;i<nums.size();i++){
-        //Step-1
-        sum+=nums[i];  
-        // Step-2
-        if(sum > result){
-            result=sum;
+        for(int i=1;i<nums.size();i++){
+            curr = max(nums[i],curr+nums[i]);
+            res = max(res, curr);
         }
-        //Step-3
-        if(sum < 0){
-            sum=0;
-        }
-    }           
-    return result;
+        return res;
     }
 };
