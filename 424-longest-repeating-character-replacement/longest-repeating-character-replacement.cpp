ba2f92035ec9eb3fd10658c92a072736a8061aa0
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2026-03-24 11:03:32 +0530

    Time: 8 ms (35.57%) | Memory: 10.8 MB (92.71%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -3,8 +3,8 @@ public:
     int characterReplacement(string s, int k) {
         int n = s.length();
         int freq[26] = {0};
-        int ws = 0;
-        int mf = 0;
+        int ws = 0; //Windows Size Track
+        int mf = 0; //for Maximum Frequency Track
         int ans = INT_MIN;
 
         int left = 0;
@@ -12,12 +12,14 @@ public:
         for (int right = 0; right < n; right++) {
 
             freq[s[right] - 'A']++;
-            mf = max(mf, freq[s[right] - 'A']);
+            mf = max(mf, freq[s[right] - 'A']); //finding max Freq
 
-            ws = right - left + 1;
+            ws = right - left + 1; // finding max window size
 
             while (ws - mf > k && left < right) {
                 freq[s[left] - 'A']--;
+                // yaha mf change nhi hoga (confusion)
+                // becuase mf = window me kisi bhi character ki maximum frequency
                 left++;
                 ws = right - left + 1;
             }
@@ -27,4 +29,9 @@ public:
 
         return ans;
     }
+
+//     mf = "maximum frequency ever seen in window expansion"
+//     👉 matlab:
+//     mf sirf badhega
+//     kabhi ghatega nahi
 };
