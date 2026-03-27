60c61f49bac35b7458b8fe114a000ef687fac99e
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2026-03-27 10:40:06 +0530

    Time: 1 ms (28.07%) | Memory: 27.2 MB (76.81%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -3,39 +3,20 @@ public:
     int maximumSum(vector<int>& arr) {
         //I will solve using two cases:
         //1-case: with NoDeletion
-        //2-case: with OneDeletion
+        //2-case: with OneDeletion (2 options -> ya to phle del kr chuke ho + arr[i],
+        // ya abhi vala del kr do and (nodeDeletion) lelo abhi tak)
         //res = max(NoDeletion, OneDeletion)
 
-        // int oneDeletion = INT_MIN; // Initial me koi element nhi hai to
-        // int noDeletion = arr[0];
-        // int res = arr[0];
-
-        // for(int i=1;i<arr.size();i++){
-        //     int prevNoDeletion = noDeletion;
-        //     int prevOneDeletion = oneDeletion;
-
-        //     noDeletion = max(arr[i], noDeletion+arr[i]);
-
-        //     int temp;
-        //     if(prevOneDeletion==INT_MIN)
-        //     temp = arr[i];
-        //     else
-        //     temp = prevOneDeletion + arr[i];
-        //     oneDeletion = max(temp, prevNoDeletion);
-
-        //     res = max(res, max(oneDeletion, noDeletion));
-        // }
-        // return res;
-        
-    int keep = arr[0];   // no deletion
-    int del = 0;         // one deletion
+    int noDeletion = arr[0];   // no deletion
+    int oneDeletion = 0;   // one deletion
     int res = arr[0];
 
     for(int i = 1; i < arr.size(); i++){
-        del = max(del + arr[i], keep);   // delete or extend
-        keep = max(arr[i], keep + arr[i]); // normal kadane
+        //oneDeletion phle hi likha jayega because noDeletion phle vala use ho rha hai
+        oneDeletion = max(oneDeletion + arr[i], noDeletion);   // (phle del chuke hai, arr[i] del kiya)
+        noDeletion = max(arr[i], noDeletion + arr[i]); // normal kadane
         
-        res = max(res, max(keep, del));
+        res = max(res, max(noDeletion, oneDeletion));
     }
     return res;
     }
