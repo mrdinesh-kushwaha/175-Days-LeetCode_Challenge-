68e0da2cd52fbaf4a45d99044ddab259654a94ef
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2026-03-27 10:11:50 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (76.81%) - LeetSync

---
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumSum(vector<int>& arr) {
+        //I will solve using two cases:
+        //1-case: with NoDeletion
+        //2-case: with OneDeletion
+        //res = max(NoDeletion, OneDeletion)
+
+        int oneDeletion = INT_MIN; // Initial me koi element nhi hai to
+        int noDeletion = arr[0];
+        int res = arr[0];
+
+        for(int i=1;i<arr.size();i++){
+            int prevNoDeletion = noDeletion;
+            int prevOneDeletion = oneDeletion;
+
+            noDeletion = max(arr[i], noDeletion+arr[i]);
+
+            int temp;
+            if(prevOneDeletion==INT_MIN)
+            temp = arr[i];
+            else
+            temp = prevOneDeletion + arr[i];
+            oneDeletion = max(temp, prevNoDeletion);
+
+            res = max(res, max(oneDeletion, noDeletion));
+        }
+        return res;
+    }
+};
