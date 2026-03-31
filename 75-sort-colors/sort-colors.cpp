49e632cf92cedb4dabc1cc1b8c40ef21504d3efa
Dinesh Kushwaha <115063563+mrdinesh-kushwaha@users.noreply.github.com>
2026-03-31 23:17:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (48.09%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-      // Solved using dutch's flag Algorithm
-      int n = nums.size();
-      int left=0;
-      int mid = 0;
-      int right = n-1;  
-
-      while(mid<=right){
-        //case-1
+    // Solved using dutch's flag Algorithm
+    int left = 0;
+    int mid = 0;
+    int right = nums.size()-1;
+    while(mid<=right){
+        //only 3 case possible
         if(nums[mid]==0){
             swap(nums[left],nums[mid]);
             left++;
             mid++;
         }
-        //Case-2
-        else if(nums[mid]==1){
-            mid++;
-        }
-        //Case-3
-        else{ // nums[mid]==2
+        else if(nums[mid]==1) mid++;
+        else { //nums[mid]==2
             swap(nums[mid],nums[right]);
             right--;
         }
-      }  
+    }
     }
 };
